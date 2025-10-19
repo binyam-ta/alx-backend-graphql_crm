0f5d2aed9b0bee187c5d92ec437f7bade1710d01
binyam-ta <binyamseleshe12@gmail.com>
2025-10-19 19:02:36 +0300

    mutations

--- crm/schema.py
+++ crm/schema.py
@@ -127,3 +127,9 @@ class CreateOrder(graphene.Mutation):
         order.save()  # calculates total_amount
 
         return CreateOrder(order=order)
+
+class Mutation(graphene.ObjectType):
+    create_customer = CreateCustomer.Field()
+    bulk_create_customers = BulkCreateCustomers.Field()
+    create_product = CreateProduct.Field()
+    create_order = CreateOrder.Field()
